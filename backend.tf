028d174039ffcb2b98745d326ab9867a6399afb0
HaneesH1994 <94160411+HaneesH1994@users.noreply.github.com>
2022-02-15 10:24:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "devops8-9"
+    bucket = "blackdog96"
     region = "us-east-1"
-    key = "test/terraform.tfstate"
+    key = "whisky/terraform.tfstate"
   }
 }
 
